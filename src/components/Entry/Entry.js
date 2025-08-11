e8c04015747caac06ad0e39e6c53954f0b5b0af8
martascorreia <correiamarta2@gmail.com>
2025-08-11 22:34:24 +0100

    im not sure what this is

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -3,7 +3,7 @@ import "./Entry.css";
 import "../../index.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Entry = ({ identification, logo, title, date, type, description, institution, startDate, endDate, icons, subEntries }) => {
+const Entry = ({ identification, description, icons, subEntries }) => {
 
   return (
     <div className="Entry">
@@ -13,18 +13,21 @@ const Entry = ({ identification, logo, title, date, type, description, instituti
       <div className="subEntryMain">
         {subEntries}
       </div>
-      {/*  < div id="icons">
-        {icons && icons.map(icon =>
-          <div>
-            <FontAwesomeIcon icon={icon.logo} />
-            <a>{icon.text}</a>
-          </div>)*/}
       {description &&
         <div className="entryDescription">
           <div dangerouslySetInnerHTML={{ __html: description }} />
         </div>
       }
+       < div id="icons">
+        {icons && icons.map(icon => {
+          <div>
+            <FontAwesomeIcon icon={icon.logo} />
+            <a>{icon.text}</a>
+          </div>})}
+      </div>
     </div>
+
+
   );
 }
 
--- src/components/TextButton/TextButton.js
+++ src/components/TextButton/TextButton.js
@@ -5,7 +5,19 @@ import "../../index.css"
 const TextButton = ({ text, elementId, className }) => {
 
   const onClick = () => {
-    if(elementId) document.getElementById(elementId).scrollIntoView({ behavior: 'smooth' })
+    if(elementId){
+      //scroll
+      var elem = document.getElementById(elementId);
+      elem.scrollIntoView({ behavior: 'smooth' });
+
+      //open section, if not already open
+      var titleSection = elem.querySelector("#titleSection");
+      var sectionClass = elementId + "ID";
+      var target = document.querySelector("." + sectionClass);
+      if (titleSection && target && target.style.display === 'none') {
+        titleSection.click();
+      }
+    } 
   };
 
   return (
